aeb4a4ee5116dbce874bd4ed164133aae5471256
vir <you@example.com>
2026-02-27 16:34:42 +0800

    -

---
+++ src/consts.ts
@@ -0,0 +1,9 @@
+export const SITE_NAME = 'Craft Parser';
+
+export const INDEX_TITLE = '';
+export const INDEX_DESCR = '';
+export const INDEX_H1    = '';
+
+export const POSTS_TITLE = '';
+export const POSTS_DESCR = '';
+export const POSTS_H1    = '';
