15877a363582fb2d2803ff319dc3c92b60410d2f
Name <you@example.com>
2026-02-27 22:13:02 +0800

    -

--- src/consts.ts
+++ src/consts.ts
@@ -1,9 +1,9 @@
 export const SITE_NAME = 'Craft Parser';
 
-export const INDEX_TITLE = '';
-export const INDEX_DESCR = '';
-export const INDEX_H1    = '';
+export const INDEX_TITLE = 'Index_title';
+export const INDEX_DESCR = 'Index_descr';
+export const INDEX_H1    = 'Index_h1';
 
-export const POSTS_TITLE = '';
-export const POSTS_DESCR = '';
-export const POSTS_H1    = '';
+export const POSTS_TITLE = 'Posts_title';
+export const POSTS_DESCR = 'Posts_descr';
+export const POSTS_H1    = 'Posts_h1';
