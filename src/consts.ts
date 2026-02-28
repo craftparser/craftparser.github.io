d45fa461f4c46b52e55fec043840c468247b2532
vir <you@example.com>
2026-02-28 13:51:03 +0800

    -

--- src/consts.ts
+++ src/consts.ts
@@ -1,9 +1,15 @@
-export const SITE_NAME = 'Craft Parser';
+export const SITE_NAME = "Craft Parser";
 
-export const INDEX_TITLE = 'Craft Parser — Парсинг на заказ';
-export const INDEX_DESCR = 'Спарсим любую необходимую информацию';
-export const INDEX_H1    = 'Craft Parser';
+export const INDEX_TITLE = "Craft Parser — Парсинг на заказ";
+export const INDEX_DESCR = "Спарсим любую необходимую информацию";
+export const INDEX_H1    = "Craft Parser";
 
-export const POSTS_TITLE = 'Craft Parser — Все посты';
-export const POSTS_DESCR = 'Блог о парсинге, веб-скрапинге и обработке данных';
-export const POSTS_H1    = 'Все посты';
+export const POSTS_TITLE = "Craft Parser — Все посты";
+export const POSTS_DESCR = "Блог о парсинге, веб-скрапинге и обработке данных";
+export const POSTS_H1    = "Все посты";
+
+export const HEADER_NAV = [
+    {label: "Test1", href: "#test1"},
+    {label: "Test2", href: "#test2"},
+    {label: "Test3", href: "#test3", select: true},
+];
