6aa1eed0c42304b514855ae808b5f0a3e0dc4b52
vir <you@example.com>
2026-02-28 21:36:14 +0800

    og_image

--- src/consts.ts
+++ src/consts.ts
@@ -1,12 +1,14 @@
 export const SITE_NAME   = "Craft Parser";
 export const ACTION_LINK = "https://t.me/craftparser"
 
-export const INDEX_TITLE = "Заказать парсинг данных — Craft Parser: сбор информации с сайтов";
-export const INDEX_DESCR = "Профессиональный парсинг данных на заказ. Собираем информацию любой сложности: товары интернет-магазинов, цены, контакты и аналитику в удобном формате (Excel, JSON, CSV).";
+export const INDEX_TITLE    = "Заказать парсинг данных — Craft Parser: сбор информации с сайтов";
+export const INDEX_DESCR    = "Профессиональный парсинг данных на заказ. Собираем информацию любой сложности: товары интернет-магазинов, цены, контакты и аналитику в удобном формате (Excel, JSON, CSV).";
+export const INDEX_OG_IMAGE = "/imgs/og-index.webp";
 
-export const POSTS_TITLE = "Блог о парсинге и веб-скрапинге — Полезные статьи | Craft Parser";
-export const POSTS_DESCR = "Читайте экспертные статьи о веб-скрапинге, автоматизации сбора данных и обработке информации. Гайды, инструменты и кейсы от команды Craft Parser.";
-export const POSTS_H1    = "Блог о парсинге и анализе данных";
+export const POSTS_TITLE    = "Блог о парсинге и веб-скрапинге — Полезные статьи | Craft Parser";
+export const POSTS_DESCR    = "Читайте экспертные статьи о веб-скрапинге, автоматизации сбора данных и обработке информации. Гайды, инструменты и кейсы от команды Craft Parser.";
+export const POSTS_OG_IMAGE = "/imgs/og-posts.webp";
+export const POSTS_H1       = "Блог о парсинге и анализе данных";
 
 export const POST_ACT_TITLE = "Нужны данные, но не знаете с чего начать?";
 export const POST_ACT_SUB   = "Пришлите ссылку, и мы подготовим бесплатный тестовый файл с вашими собранными данными для оценки качества.";
